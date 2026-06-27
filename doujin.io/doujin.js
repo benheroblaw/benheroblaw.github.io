ac6076debcae03f6ee185dd3e7f9aa8cb59e44b9
benheroblaw <benheroblade@gmail.com>
2026-06-27 14:49:39 -0500

    will this work

--- doujin.io/doujin.js
+++ doujin.io/doujin.js
@@ -2,7 +2,7 @@ document.title = document.title.replace('J18 Free-Reader', 'doujin.io')
 var link = document.querySelector("link[rel~='icon']");
 if (!link) {
     link = document.createElement('link');
-    link.rel = 'icon';
+    link.rel = 'shortcut icon';
     document.head.appendChild(link);
 }
 link.href = 'https://benheroblaw.github.io/icons/J18-logo-purple-32x32.png';
