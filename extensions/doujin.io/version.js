f014b8fbb45c32f5a23e795b31028da559fe3dbe
benheroblaw <benheroblade@gmail.com>
2026-06-27 21:28:13 -0500

    doujin.io title 1.4.2

--- extensions/doujin.io/version.js
+++ extensions/doujin.io/version.js
@@ -1,21 +1,18 @@
 let version = '1.4.2'
+
 console.log('setting interval...')
 setInterval(() => {
+  try {
   let verElement = document.getElementById('doujinVer')
   let newestVer = document.getElementById('doujinNewest').textContent
-  // try {
     if (version === newestVer) {
-      verElement = `Current Version: ${version}!`
+      verElement.textContent = `Current Version: ${version}!`
       verElement.style.color = '#0f0'
     }
     else {
-      verElement.innerHTML = 'Current Version: ' + version
+      verElement.textContent = 'Current Version: ' + version
+      verElement.style.color = '#fff'
     }
-  // }
-  // catch {}
-
-  // console.log('setting version')
+  }
+  catch {}
   }, 100);
-
-// document.title += ' version 1.4.2'
-// document.getElementById('doujinVer').innerHTML += 'Current Version: 1.4.2'
