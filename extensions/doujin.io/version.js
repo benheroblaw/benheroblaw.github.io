982c5dd5203bfa5e3d041fad980e2a03504220cd
benheroblaw <benheroblade@gmail.com>
2026-06-27 21:00:56 -0500

    extensions text

--- extensions/doujin.io/version.js
+++ extensions/doujin.io/version.js
@@ -1,1 +1,21 @@
-document.querySelector('#doujinVer').innerText = "Current Version: 1.4.2"
+let version = '1.4.2'
+console.log('setting interval...')
+setInterval(() => {
+  let verElement = document.getElementById('doujinVer')
+  let newestVer = document.getElementById('doujinNewest').textContent
+  // try {
+    if (version === newestVer) {
+      verElement = `Current Version: ${version}!`
+      verElement.style.color = '#0f0'
+    }
+    else {
+      verElement.innerHTML = 'Current Version: ' + version
+    }
+  // }
+  // catch {}
+
+  // console.log('setting version')
+  }, 100);
+
+// document.title += ' version 1.4.2'
+// document.getElementById('doujinVer').innerHTML += 'Current Version: 1.4.2'
